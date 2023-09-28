232a71023c1df73ae512aa981d81a40c54f12a0f
devgenx <107775878+DevgenX@users.noreply.github.com>
2023-09-28 16:37:02 -0700

    retrieving OHLCV with yfinance API

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+
+import yfinance
+import pytz
 
 
 def get_sp500_tickers():
     res = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
-    soup = BeautifulSoup(res.content, "lxml")
+    soup = BeautifulSoup(res.content, "html")
     table = soup.find_all("table")[0]
     df = pd.read_html(str(table))
     tickers = list(df[0].Symbol)
@@ -15,9 +19,36 @@ def get_sp500_tickers():
 tickers = get_sp500_tickers()
 
 
-def get_history(ticker):
-    pass
+def get_history(ticker, period_start, period_end, granularity="1d"):
+    df = (
+        yfinance.Ticker(ticker)
+        .history(
+            start=period_start, end=period_end, interval=granularity, auto_adjust=True
+        )
+        .reset_index()
+    )
+    df = df.rename(
+        columns={
+            "Date": "datetime",
+            "Open": "open",
+            "High": "high",
+            "Low": "low",
+            "Close": "close",
+            "Volume": "volume",
+        }
+    )
+    if df.empty:
+        return pd.DataFrame()
 
+    df["datetime"] = df["datetime"].dt.tz_convert(pytz.utc)
+    df = df.drop(columns=["Dividends", "Stock Splits"])
+    df = df.set_index("datetime", drop=True)
+    return df
+
+
+period_start = datetime(2010, 1, 1, tzinfo=pytz.utc)
+period_end = datetime(2020, 1, 1, tzinfo=pytz.utc)
 
 for ticker in tickers:
-    get_history(ticker)
+    df = get_history(ticker, period_start, period_end)
+    print(ticker, df)

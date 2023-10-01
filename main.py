f595055ae7f5180c662305e3b19184bae9f5615c
devgenx <107775878+DevgenX@users.noreply.github.com>
2023-10-01 14:51:16 -0700

    added multithreading to optimize API calls on getting historical data

--- main.py
+++ main.py
@@ -5,18 +5,16 @@ from datetime import datetime
 
 import yfinance
 import pytz
+import threading
 
 
 def get_sp500_tickers():
     res = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
-    soup = BeautifulSoup(res.content, "html")
+    soup = BeautifulSoup(res.content, features="lxml")
     table = soup.find_all("table")[0]
     df = pd.read_html(str(table))
     tickers = list(df[0].Symbol)
     return tickers
-
-
-tickers = get_sp500_tickers()
 
 
 def get_history(ticker, period_start, period_end, granularity="1d"):
@@ -46,9 +44,36 @@ def get_history(ticker, period_start, period_end, granularity="1d"):
     return df
 
 
+def get_histories(tickers, period_starts, period_ends, granularity="1d"):
+    dfs = [None] * len(tickers)
+
+    def _helper(i):
+        df = get_history(
+            tickers[i], period_starts[i], period_ends[i], granularity=granularity
+        )
+        dfs[i] = df
+
+    threads = [threading.Thread(target=_helper, args=(i,)) for i in range(len(tickers))]
+    print(threads)
+    [thread.start() for thread in threads]
+    [thread.join() for thread in threads]
+    # filter out tickers w/o data
+    tickers = [tickers[i] for i in range(len(tickers)) if not dfs[i].empty]
+    dfs = [df for df in dfs if not df.empty]
+    return tickers, dfs
+
+
+def get_ticker_dfs(start, end):
+    tickers = get_sp500_tickers()
+    starts = [start] * len(tickers)
+    ends = [end] * len(tickers)
+    tickers, dfs = get_histories(tickers, starts, ends, granularity="1d")
+    return tickers, {ticker: df for ticker, df in zip(tickers, dfs)}
+
+
 period_start = datetime(2010, 1, 1, tzinfo=pytz.utc)
 period_end = datetime(2020, 1, 1, tzinfo=pytz.utc)
 
-for ticker in tickers:
-    df = get_history(ticker, period_start, period_end)
-    print(ticker, df)
+
+tickers, ticker_dfs = get_ticker_dfs(start=period_start, end=period_end)
+print(ticker_dfs)
